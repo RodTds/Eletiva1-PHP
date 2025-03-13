ff9f62af7e2595bf58107d3ec5153f366e2206b4
RodTds <rodrigo.silva505@fatec.sp.gov.br>
2025-03-13 18:30:55 -0300

    Lista 5 corrigida

--- Lista5/config.php
+++ Lista5/config.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -9,10 +8,25 @@
   </head>
   <body>
     <h1>Configurações</h1>
-    <button>Encerrar Sessão 
+    <!-- Formulário para acionar o botão -->
+    <form method="POST">
+        <button type="submit" name="encerrar_sessao" class="btn btn-danger">Encerrar Sessão</button>
+    </form>
+
     <?php
-       session_destroy();
-    ?></button>
+       // Função para limpar a sessão
+       function limparSessao(): void {
+        
+           session_destroy(); // Destrói a sessão
+           echo "<p>Sessão encerrada!</p>";
+       }
+
+       // Verifica se o botão foi pressionado
+       if (isset($_POST['encerrar_sessao'])) {
+           limparSessao(); // Chama a função para destruir a sessão
+       }
+    ?>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
