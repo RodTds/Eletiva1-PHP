6ea5128583b037ebf0cff2f9fcda74a5c125c1ea
RodTds <rodrigo.silva505@fatec.sp.gov.br>
2025-03-23 20:38:25 -0300

    atualizado ate o exercicio 3 pronto

--- Lista5/exercicio2.php
+++ Lista5/exercicio2.php
@@ -1,18 +1,88 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercicio 2</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-      <form action="" method="post">
-            <?php for($i=0;$i <5;$i++):?>
-                <input type ="text" name></input>
-            <?php endfor;?>
-      </form>
-        
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Exercicio 2</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+    <form action="" method="post" class="w-50">
+      
+      <?php for ($i = 0; $i < 2; $i++): ?>
+        <div class="row justify-content-center">
+
+          <h2 class="text-center mb-4">Dados do Aluno</h2>
+
+          <div class="mb-3">
+            <label for="nome" class="form-label">Informe o nome</label>
+            <input type="text" name="nome[]" class="form-control" required>
+          </div>
+          <div class="mb-3">
+            <label for="n1" class="form-label">Informe a nota 1</label>
+            <input type="number" name="n1[]" class="form-control" required>
+          </div>
+          <div class="mb-3">
+            <label for="n2" class="form-label">Informe a nota 2</label>
+            <input type="number" name="n2[]" class="form-control" required>
+          </div>
+          <div class="mb-3">
+            <label for="n3" class="form-label">Informe a nota 3</label>
+            <input type="number" name="n3[]" class="form-control" required>
+          </div>
+        </div>
+      <?php endfor; ?>
+
+      <div class="mb-3 text-center">
+        <button type="submit" class="btn btn-primary">Enviar</button>
+      </div>
+    </form>
+  </div>
+
+
+  <div class="container d-flex justify-content-center">
+    <div class="col-6">
+      <?php
+      if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        try {
+          $alunos = array();  //  array de alunos
+
+          // recebendo os dados dos alunos do inputs
+          for ($i = 0; $i < 2; $i++) {
+            $nome = $_POST['nome'][$i];
+            $nota1 = $_POST['n1'][$i];
+            $nota2 = $_POST['n2'][$i];
+            $nota3 = $_POST['n3'][$i];
+            $media = ($nota1 + $nota2 + $nota3) / 3;
+            $alunos[] = array(
+              "nome" => $nome,
+              "media" => $media
+            );
+          }
+
+          // Ordenar os alunos pela média (em ordem crescente)
+          usort($alunos, function ($a, $b) {
+            return $a['media'] <=> $b['media'];
+          });
+
+
+          echo "<ul class='list-group'>";
+          foreach ($alunos as $aluno) {
+            echo "<li class='list-group-item'>Nome: " . $aluno['nome'] . " -- Média: " . number_format($aluno['media'], 2, ',', '.') . "</li>";
+          }
+          echo "</ul>";
+        } catch (Exception $e) {
+          echo  $e->getMessage();
+        }
+      }
+      ?>
+    </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
 </html>
---
+++ Lista5/exercicio3.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercicio 3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <form action="" method="post">
+
+        <?php for ($i = 0; $i < 3; $i++): ?>
+
+            <div class="container">
+               
+                <div class="row justify-content-center">
+                    <h2 class="col-12 text-center mb-4">Dados do Produto</h2> 
+
+                    <div class="col-6">
+                        <div class="mb-3">
+                            <label for="codigo">Informe o Código</label>
+                            <input type="number" name="codigo[]" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="nome">Informe o Nome</label>
+                            <input type="text" name="nome[]" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="preco">Informe o Preço</label>
+                            <input type="number" name="preco[]" step="0.01" class="form-control">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endfor; ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-1 mx-auto ">
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-6 mx-auto">
+                <?php
+                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                    try {
+
+                        $produtos = array();  
+
+                        for ($i = 0; $i < 3; $i++) {
+                            $codigo = strtoupper( $_POST['codigo'][$i]);
+                            $nome = strtoupper($_POST['nome'][$i]);
+                            $preco = strtoupper($_POST['preco'][$i]);
+
+                            $produtos[] = array(
+                                "codigo" => $codigo,
+                                "nome" => $nome,
+                                "preco" => $preco
+                            );
+                        }
+                        usort($produtos, function ($a, $b) {
+                            return strcmp($a['nome'], $b['nome']);  // Ordena em ordem alfabética pelo nome
+                        });
+                        foreach ($produtos as $prod) {
+                            echo "<br> Codigo: " . $prod['codigo'] . " -- Nome: " . $prod['nome'] . " -- Preço: " . number_format($prod['preco']);
+                            echo "<br>-----------------------------------------------------------------------------";
+                        }
+                    } catch (Exception $e) {
+                        echo  $e->getMessage();
+                    }
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
